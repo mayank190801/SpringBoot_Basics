d4eae3f074c25c0e916bf76bc238e7efd10acf1c
Mayank Singh <mayank190801@gmail.com>
2023-08-04 14:51:30 +0530

    feat: starting new project

---
+++ secondApp/src/main/java/com/mayank/secondApp/SecondAppApplication.java
@@ -0,0 +1,18 @@
+package com.mayank.secondApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecondAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecondAppApplication.class, args);
+	}
+
+	//let's learn this thing
+
+
+
+
+}
---
+++ secondApp/src/main/java/com/mayank/secondApp/rest/FunRestController.java
@@ -0,0 +1,4 @@
+package com.mayank.secondApp.rest;
+
+public class FunRestController {
+}
