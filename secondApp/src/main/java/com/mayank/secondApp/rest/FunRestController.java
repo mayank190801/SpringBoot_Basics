85b5c9b7402f86961feefb8de075615a2a3d5a9c
Mayank Singh <mayank190801@gmail.com>
2023-08-04 16:50:11 +0530

    feat: getting started with actuators

--- secondApp/src/main/java/com/mayank/secondApp/rest/FunRestController.java
+++ secondApp/src/main/java/com/mayank/secondApp/rest/FunRestController.java
@@ -1,4 +1,29 @@
 package com.mayank.secondApp.rest;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class FunRestController {
+
+    //end point (start)
+    @GetMapping("/")
+    public String sayHello(){
+        return "Hello world!";
+    }
+    @GetMapping("/fortune")
+    public String getFortune(){
+        return "Today is your lucky day!";
+    }
+
+    //made some change
+
+
+
+
+
+
+
+
+
 }
